249361998033232ab14d8f84cdf8ed1059bb3d1a
Billy Rhoades <brhoa@monsanto.com>
2014-07-17 16:51:42 -0500

    End of day, another cleanup commit + doc.

---
+++ conversion_files/includes/seleniumWrapper/formatting.py
@@ -0,0 +1,56 @@
+####################################################################################################
+# format( t )
+# Formats a Float into a displayable form (rounds it and converts to string)
+def format( t ):
+    return str( round( t, 2 ) )
+####################################################################################################
+
+
+####################################################################################################
+# formatError( res )
+# Formats an Error Message
+#   Previously formatted an error message provided by ChromeDriver via regex.
+def formatError( res ):
+    a = re.compile(r"Message: [a-z]'([A-Za-z\s\:]+)\\n")             # Currently in the format for chromeDriver
+                                                                     #FIXME: Should read JSON from GhostDriver
+    if a.match( str( res ) ):
+        r = a.match( str( res ) )
+        return "ERROR: \"" + r.groups( )[0] + "\""
+    else:
+        if len( res ) > 40:
+            res = res[0:40]
+        return res
+####################################################################################################
+
+####################################################################################################
+# childMessage( num, msg )
+# Formats a message for child to be printed out.
+def childMessage( num, msg ):
+    print( "Child #" + str( num + 1 ) + ": " + msg )
+####################################################################################################
+
+####################################################################################################
+# stats( good, bad, timetaken, children, times )
+# Prints Statistics
+#   Prints out success / fail counts, total / remaining orders, failure rate, number of children,
+#   and then averages / extrapolations. It requires good / bad count (#), and then arrays of times taken,
+#   the child processes, and number of times (#). 
+def stats( good, bad, timetaken, children, times ):   
+    print( "\n" + ("="*40) )
+    print( "Successful: " + str(good) + (" "*3) + "Failed: " + str(bad) )
+    print( "Total: " + str(good+bad) + (" "*3) + "Remaining: " + str(times*len(children)-good) )
+    print( "Failure Rate: " + format(bad/float(good+bad)*100) + "%" );
+    print( "Children: " + str( len( children ) ) )
+    avg = 0
+    for time in timetaken:
+        avg += time
+    if len(timetaken) > 0:
+        avg /= len(timetaken)
+        print( "Average / Estimates:" )
+        print( "  Time per order: " + format(avg) + " seconds" )
+        print( "  Orders/s: " + format(1/avg) + (" "*3) + "Orders/m: "  + format(60/avg) + (" "*3)+ "Orders/hr: " 
+               + format(60*60/avg) + (" "*3) + "Orders/day: " + format(60*60*24/avg) )
+    else:
+        print "No data to extrapolate or average from"
+    print( ( "="*40 ) + "\n" )
+####################################################################################################
--- conversion_files/includes/seleniumWrapper/wrapper.py
+++ conversion_files/includes/seleniumWrapper/wrapper.py
@@ -4,7 +4,17 @@ import re, sys, time, os
 from multiprocessing import Process, Queue
 from datetime import datetime
 
-READY = 3
+# Queue Results
+FAILED = False
+DONE = True
+READY = 2 
+
+#childstats indicies
+TIMESTAMP      = 0
+CHILD_NUMBER   = 1
+FAILURES       = 2
+SUCESSES       = 3
+ACTIVE         = 4
 
 def runFullTest( numtimes, q, func, d ):
     DesiredCapabilities.PHANTOMJS['phantomjs.page.settings.userAgent'] = 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'
@@ -12,7 +22,7 @@ def runFullTest( numtimes, q, func, d ):
     dcaps = { 'acceptSslCerts': True }
     error_path = os.path.dirname(os.path.abspath(__file__)) + "\\logs\\" + d[0] + "\\" + str(d[1]) + "\\"
     if not os.path.isdir( error_path ):
-      os.makedirs( error_path )
+        os.makedirs( error_path )
     driver = PhantomJS( desired_capabilities=dcaps, service_args=['--ignore-ssl-errors=true'], service_log_path=( error_path + "ghostdriver_" + str(d[2]) + ".log" ) )
  
     q.put( [ READY ] )
@@ -36,37 +46,6 @@ def runFullTest( numtimes, q, func, d ):
 
         q.put( res )
     driver.quit( )
-
-def stats( good, bad, timetaken, children, times ):   
-    print( "\n" + ("="*40) )
-    print( "Successful: " + str(good) + (" "*3) + "Failed: " + str(bad) )
-    print( "Total: " + str(good+bad) + (" "*3) + "Remaining: " + str(times*len(children)-good) )
-    print( "Failure Rate: " + format(bad/float(good+bad)*100) + "%" );
-    print( "Children: " + str( len( children ) ) )
-    avg = 0
-    for time in timetaken:
-        avg += time
-    if len(timetaken) > 0:
-        avg /= len(timetaken)
-        print( "Average / Estimates:" )
-        print( "  Time per order: " + format(avg) + " seconds" )
-        print( "  Orders/s: " + format(1/avg) + (" "*3) + "Orders/m: "  + format(60/avg) + (" "*3)+ "Orders/hr: " 
-               + format(60*60/avg) + (" "*3) + "Orders/day: " + format(60*60*24/avg) )
-    else:
-        print "No data to extrapolate or average from"
-    print( ( "="*40 ) + "\n" )
-
-def formatError( res ):
-    a = re.compile(r"Message: [a-z]'([A-Za-z\s\:]+)\\n")
-
-    if a.match( str( res ) ):
-        r = a.match( str( res ) )
-        return "ERROR: \"" + r.groups( )[0] + "\""
-    else:
-        return res
-
-def childMessage( num, msg ):
-    print( "Child #" + str( num + 1 ) + ": " + msg )
 
 def main( func ):
     print( "\nLibraries loaded!\n\n" )
@@ -99,51 +78,56 @@ def main( func ):
         queues.append( q )
     
     print( "\n" + ("="*40) )
-  
+
+    mainLoop( processes, queues, times, results, childstats )
+
+def mainLoop( processes, queues, times, results, childstats ):
     last = 0
     # Cycle and watch
     while len( processes ) > 0:
         time.sleep( 1 )
+
+        # Check if we have anything new to chew on
         for q in queues:
             x = queues.index( q )
+
+            # Chew on this queue until we deplete it
             while not q.empty( ):
                 res = q.get( )
-                if res[0] == False:
+
+                if res[0] == FAILED:   # This process (w/ index x) threw an exception
                     res[2] = formatError( res[2] )
                     childMessage( x, "ERROR - " + res[2] )
                     results.append( res[0] )
-                elif res[0] == True:
+                elif res[0] == DONE:  # This process reported finished with a job
                     times.append( res[1] )
                     childMessage( x, "DONE (" + format( res[1] ) + "s)" )
                     results.append( res[0] )
-                    childstats[x][3] += 1
+                    childstats[x][SUCCESSES] += 1
                 elif res[0] == READY:
                     childMessage( x, "STARTING" )
+                    next
+                results.append( res[0] )
 
         for p in processes:
             i = processes.index( p )
-            if not p.is_alive( ) and childstats[i][4]:
-                if childstats[i][3] >= numtimes: 
+            if not p.is_alive( ) and childstats[i][ACTIVE]:
+                if childstats[i][SUCCESSES] >= numtimes: 
                     childMessage( i, "FINISHED" )
-                    childstats[i][4] = False
+                    childstats[i][ACTIVE] = False
                     p.join( )
                     next
                 else:
                     childMessage( i, "DEAD" )
                 p.join( )
 
-                childstats[i][2] += 1
+                childstats[i][FAILURES] += 1
                 processes[i] = Process( target=runFullTest, args=( numtimes, queues[i], func, childstats[i] ) )
                 processes[i].start( )
                 childMessage( i, "LOADING" ) 
 
         if len(results) >= step+last:
             last = len(results)
-            good = 0
+            good = mapgithub 
             bad = 0
-            for r in results:
-                if r == True:
-                    good += 1   
-                else:
-                    bad += 1
             stats( good, bad, times, processes, numtimes )
