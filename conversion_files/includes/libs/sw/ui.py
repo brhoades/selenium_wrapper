2c321c8c3c19c91288bd179650bcffe7ee866f34
Billy Rhoades <brhoa@monsanto.com>
2014-09-02 10:24:18 -0500

    Moved options/stats into subwins

--- conversion_files/includes/libs/sw/ui.py
+++ conversion_files/includes/libs/sw/ui.py
@@ -16,12 +16,24 @@ class Ui:
         self.nextStats = time.time( )
 
         self.STATS_HEIGHT  = 7
-        self.STATS_WIDTH   = self.scr.getmaxyx( )[1]-2
+        self.STATS_WIDTH   = self.x( )-2
 
-        self.OPTIONS_HEIGHT = self.STATS_HEIGHT + 1
+        self.stats = self.scr.subwin( self.STATS_HEIGHT-1, self.STATS_WIDTH, 
+                self.y( )-self.STATS_HEIGHT, 1 )
+        
+        self.OPTIONS_HEIGHT = self.y( ) - self.STATS_HEIGHT 
         self.OPTIONS_WIDTH  = 20
 
+        self.opts = self.scr.subwin( self.OPTIONS_HEIGHT, self.OPTIONS_WIDTH, 1, 
+                self.x( ) - self.OPTIONS_WIDTH ) 
+
         self.drawMainScreen( )
+
+    def x( self ):
+        return self.scr.getmaxyx( )[1]
+
+    def y( self ):
+        return self.scr.getmaxyx( )[0]
 
     def drawMainScreen( self ):
         self.scr.border( )
@@ -30,24 +42,19 @@ class Ui:
         # Draw our title at the top
         self.scr.addstr( 0, 3, "Selenium Wrapper Console" )
 
-        # Line for key panel
-        self.scr.vline( 1, self.scr.getmaxyx( )[1]-self.OPTIONS_WIDTH, 0, self.scr.getmaxyx( )[0]-self.STATS_HEIGHT )
+        # Line for key window
+        self.scr.vline( 1, self.x( )-self.OPTIONS_WIDTH, 0, self.y( )-self.STATS_HEIGHT )
 
-        # Calculate the dimensions of our key panel
-        sx = self.scr.getmaxyx( )[1]-( self.OPTIONS_WIDTH - 2 )
-        sy = 2
         i = 0
-
         for l in self.options:
-            self.scr.addstr( sy + i*2, sx, l )
+            self.opts.addstr( i*2, 2, l )
             i += 1
 
-        self.scr.hline( self.scr.getmaxyx( )[0]-self.STATS_HEIGHT, 1, 0, self.STATS_WIDTH )
+        # Line for stats window
+        self.scr.hline( self.y( )-self.STATS_HEIGHT-1, 1, 0, self.STATS_WIDTH )
         self.scr.refresh( )
 
     def think( self ):
-        self.options = self.options
-
         # Check for keypresses
         key = self.scr.getch( )
         
@@ -72,9 +79,8 @@ class Ui:
     def updateStats( self ):
         statstrs = [ ]
 
-        # Wipe the box
-        for i in range( 1, self.STATS_HEIGHT-1 ):
-            self.scr.addstr( self.scr.getmaxyx( )[0]-self.STATS_HEIGHT+i, 1, " "*self.STATS_WIDTH )
+        # Clear our window
+        self.stats.clear( )
 
         # Number of Children
         statstrs.append( ''.join( [ "Children: ", str( len( self.pool.children ) ) ] ) )
@@ -99,14 +105,14 @@ class Ui:
         times = self.pool.timeTaken( )
         statstrs.append( ''.join( [ "Avg Job: ", format( avg( times ) ), "s" ] ) )
 
-        adj = 2
-        k = 0
+        adj = 2 # Amount we are shifting right in characters
+        k = 0   # Amount we are shifting vertically
         for st in statstrs:
-            stl = len( st )
+            stl = len( st ) # String length
             if ( adj + stl ) >= self.STATS_WIDTH:
                 k += 1
                 adj = 2
-            self.scr.addstr( self.scr.getmaxyx( )[0]-self.STATS_HEIGHT+( 1 + 1*k ), adj, st )
+            self.stats.addstr( k, adj, st )
             adj += stl + 3
-        self.scr.refresh( )
+        self.stats.refresh( )
 
