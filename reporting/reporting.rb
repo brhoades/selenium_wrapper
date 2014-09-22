9f9ebae37c9352d40a4d1c64c768cb7392d3c6be
Billy J Rhoades <brhoa@monsanto.com>
2014-09-22 14:00:36 -0500

    Basic functionality up, along with row creation.

--- reporting/reporting.rb
+++ reporting/reporting.rb
@@ -2,6 +2,16 @@ require 'sinatra'
 require 'sqlite3'
 require 'json'
 
+# Reporting Constants
+R_START             = 0
+R_JOB_START         = 1
+R_JOB_COMPLETE      = 2
+R_JOB_FAIL          = 3
+R_STOP              = 4
+R_ALIVE             = 5
+R_NEW_CHILD         = 6
+R_END_CHILD         = 7
+
 db = SQLite3::Database.new "reports.db"
 
 set :bind, '0.0.0.0'
@@ -12,5 +22,51 @@ end
 
 post '/report' do
   # Incoming data is JSON
-  print "\n\n", JSON.parse( request.body.read.to_s ), "\n\n"
+  payload =  JSON.parse( request.body.read.to_s )
+  payload = payload['payload']
+
+  payload.each do |p|
+    id = db.get_first_value "SELECT id FROM clients WHERE name=?", p['id']
+    print "Got payload for '", p['id'], "' with type '", p['type'], "'.\n"
+
+    case p['type']
+      when R_START
+        if id == nil
+          db.execute "INSERT INTO clients (name,active,lastping) VALUES (?,?,?)", [ p['id'], 1, Time.now.to_i ]
+          id = db.get_first_value "SELECT id FROM clients WHERE name=?", p['id']
+          print "New client '", p['id'], "'\n"
+        else
+          db.execute "UPDATE clients SET active=? WHERE id=?", [ 1, id ]
+          print "Client '", p['id'], "' initialized\n"  
+        end
+        status 200
+      when R_JOB_START
+        next if id == nil
+        status 200
+
+      when R_JOB_COMPLETE
+        next if id == nil
+        status 200
+
+      when R_JOB_FAIL
+        next if id == nil
+        status 200
+
+      when R_STOP
+        next if id == nil
+        status 200
+
+      when R_ALIVE
+        next if id == nil
+        status 200
+
+      when R_NEW_CHILD
+        next if id == nil 
+        status 200
+      
+      when R_END_CHILD
+        next if id == nil
+        status 200
+    end
+  end
 end
