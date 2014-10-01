1b3b3c36f35317dc624963f4eded5d4da330c3bf
Billy J Rhoades <brhoa@monsanto.com>
2014-10-01 15:04:39 -0500

    Timing out preserves some children's endtime now

--- reporting/scheduling.rb
+++ reporting/scheduling.rb
@@ -3,13 +3,16 @@ require 'json'
 
 $sched = Rufus::Scheduler.new
 
+######################
+# TIMEOUT CHECK
+#
 $sched.every '15s' do
   # Look at active runs
   $db.execute( "SELECT id,starttime FROM runs WHERE endtime=-1" ) do |rid, runstarttime|
     $db.execute( "SELECT L.id,L.lastping,L.name FROM children AS C, clients as L WHERE C.rid=? AND C.endtime=-1 AND l.id=C.cid", rid ) do |cid, lastping, name|
       next if Time.now.to_i - lastping < CLIENT_TIMEOUT # Likely in progress
 
-      $db.execute "UPDATE children SET endtime=? WHERE rid=? AND cid=?", [ Time.now.to_i, rid, cid ]
+      $db.execute "UPDATE children SET endtime=? WHERE rid=? AND cid=? AND endtime=-1", [ Time.now.to_i, rid, cid ]
       #FIXME Could be other clients, don't die off yet
       $db.execute "UPDATE runs SET endtime=? WHERE id=?", [ Time.now.to_i, rid ]
 
