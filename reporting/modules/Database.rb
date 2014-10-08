ffe63bc0283cfbabe342b313a08a0f04b0086c05
Billy J Rhoades <brhoa@monsanto.com>
2014-10-08 09:06:15 -0500

    Implemented wrapper for sqlite to mysql2
    Switched to the mysql2 gem

--- reporting/modules/Database.rb
+++ reporting/modules/Database.rb
@@ -1,30 +1,51 @@
-require 'mysql'
+require 'mysql2'
 
 class Database
   def initialize( cfg )
     @cfg = cfg['db']
-    @db = Mysql.new @cfg['host'], @cfg['user'], @cfg['pass'], @cfg['name']
+    @db = Mysql2::Client.new host: @cfg['host'], username: @cfg['user'], password: @cfg['pass'], database: @cfg['name']
   end
 
-  def execute( query, args=[] )
+  def execute( query, args=[], opts={} )
     # Must be an array
     if not args.is_a? Array
       args = [ args ]
     end
 
+    if opts.size == 0
+      opts[:as] = :array
+    end
+
     # Substite values into query securely
     query.gsub!(/\?/) do |s|
-      @db.escape_string args.shift.to_s
+      @db.escape args.shift.to_s
     end
 
     if args.size > 0
       print "ERROR: More args than ?'s (", args.size, ")\n"
     end
 
-    @db.query query
+    r = @db.query( query, opts ).to_a
+
+    # flatten if doesn't need to be 2d
+    r.map! do |i|
+      if i.is_a? Array and i.size == 1
+        i.first
+      else
+        i
+      end
+    end
+
+    r
   end
 
-  def get_first_value( query, args=[] )
-    self.execute( query, args ).first
+  def get_first_value( query, args=[], opts={} )
+    r = self.execute( query, args, opts )
+
+    if r.is_a? Array
+      r.first
+    else
+      r
+    end
   end
 end 
