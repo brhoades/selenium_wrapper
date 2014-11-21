14e1f0623a0b1b30db787ba4911c3544d53e5b7b
Billy Rhoades <brhoa@monsanto.com>
2014-11-21 12:16:21 -0600

    Improved ElementCache doc + added expiration

--- conversion_files/includes/libs/sw/cache.py
+++ conversion_files/includes/libs/sw/cache.py
@@ -1,8 +1,42 @@
-import time, sys
+import time
 from selenium.common.exceptions import *
 
 class ElementCache:
-    """Creates an object to store WebDriver elements on specific pages so subsequent lookups don't require scanning the DOM.
+    """Creates an object to store WebDriver elements on specific pages so subsequent lookups do not require scanning the DOM. 
+
+    A great deal of time on some pages is spent looking for elements which have already been found. If the page does not change,
+    any previously used element link will be valid (barring changed CSS). ElementCache wraps a hash table which uses the url with
+    element name and type for a key to point to a :py:class:`~selenium.webdriver.remote.webelement.WebElement`. This is in turn used 
+    for every call to :func:`~sw.utils.exists` before searching for the element (via :func:`get`), as the hash lookup is nearly free compared
+    to a page search. 
+
+    self.cache is, as mentioned above, a hash. The hash takes the following format::
+
+        { 
+            "<URL where elements are valid>" => {
+                "<element identifying characteristic>_<identifying field>" => [ <expiration unix timestamp, if any>, <`WebElement` reference to loginbutton> ],
+                [...]
+
+            },
+            [...]
+        }
+    
+    A mock example::
+    
+        { 
+            "http://website.com/pageiamon.html" => {
+                "loginbutton_id" => [ 1416592420, <`WebElement` reference to loginbutton> ],
+                "banner_name"    => [ None,       <`WebElement` reference to banner> ],
+                [...]
+
+            },
+            [...]
+        }
+                
+    
+    self.cache is effectively a double hash that points to an array. Checks are performed on the element before returned, so an invalid or expired element
+    should never be returned. At the end of every run the owning :py:class:`~sw.child.Child` object manually clears the ElementCache as, for almost all test cases,
+    the elements will be long expired.
 
     :return: ElementCache (self)
     """
@@ -10,10 +44,10 @@ class ElementCache:
         self.cache = { }
 
     def add( self, url, e, **kwargs ):
-        """Add an element to the cache. It is stored by a handle and deleted automatically after a full run.
+        """Add an element to the cache (ElementCache.cache). It is stored by a handle and deleted automatically after a full run.
 
         :param url: The URL of element e.
-        :param e: The `Webelement` to be stored.
+        :param e: The `WebElement` to be stored.
         
         :Kwargs:
           * **id** (*None*): Identifier for e on the passed url. 
@@ -47,7 +81,7 @@ class ElementCache:
           * **type** (*None*): Type of the identifier for the element in the cache, on the passed url.
           * **handle** (*None*): (separate from id/type) A handle to identify this element by internally (unique).
 
-        :return: :py:class:`~selenium.webdriver.remote.webelement.WebElement` for the cached item if a matching element exists. None if not.
+        :return: `WebElement` for the cached item if a matching element exists. None if not.
         """
         id = kwargs.get( "id", None )
         type = kwargs.get( "type", None )
@@ -65,7 +99,11 @@ class ElementCache:
         if not handle in self.cache[url]:
             return None
 
-        e = self.cache[url][handle][1]
+        data = self.cache[url][handle]
+        if data[0] <= time.time( ):
+            e = None
+        else:
+            e = data[1]
         try:
             e.get_attribute( "class" )
         except NoSuchAttributeException:
