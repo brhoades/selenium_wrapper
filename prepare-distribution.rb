80950889e280df47330be0f28d4a0bd73d4ded06
Billy Rhoades <brhoa@monsanto.com>
2014-12-08 10:22:58 -0600

    Updated distribution script

--- prepare-distribution.rb
+++ prepare-distribution.rb
@@ -2,6 +2,9 @@
 require 'FileUtils'
 
 $sz = "C:\\Program Files\\7-Zip\\7z.exe"
+$rev = File.foreach( ".git/refs/heads/master" ).first[0..6]
+
+$outputfolder = "../packaged/"
 
 # What are we doing?
 
@@ -13,64 +16,67 @@ print "Make a choice: "
 
 sel = gets.chomp.to_i
 
-begin
-  if sel == 2
-    # Gets all folders in out/
-    list = Dir["out/*"]
-    list.map! { |l| l if File.directory? l }.compact! if list.size > 0
+if sel == 2
+  # Gets all folders in out/
+  list = Dir["out/*"]
+  list.map! { |l| l if File.directory? l }.compact! if list.size > 0
 
-    i = 0
-    list.each do |l|
-      print i + 1, ") ", list[i], "\n"
-      i += 1
-    end
+  i = 0
+  list.each do |l|
+    print i + 1, ") ", list[i], "\n"
+    i += 1
+  end
 
-    print "Make a selection to package: "
-    sel = gets.chomp.to_i
+  print "Make a selection to package: "
+  sel = gets.chomp.to_i
 
-    print "\n\n"
+  print "\n\n"
 
-    out    = File.expand_path "out"
-    target = File.expand_path list[sel-1]
-    targetzip = target + ".zip"
-    logs   = File.join target, "logs"
+  out    = File.expand_path "out"
+  target = File.expand_path list[sel-1]
+  targetzip = File.join File.expand_path( $outputfolder ), ( File.basename( target ) + "-" + $rev + ".zip" )
+  logs   = File.join target, "logs"
 
-    print "Moving logs: "
-    FileUtils.mv( logs, out ) if File.exists? logs
-    print "DONE\n"
+  print "Moving logs: "
+  FileUtils.mv( logs, out ) if File.exists? logs
+  print "DONE\n"
 
-    print "Removing *.pyc files: "
-    # Remove pyc files
-    Dir['#{target}/*'].each { |f| FileUtils.rm f if f =~ /\.pyc$/ }
-    print "DONE\n"
+  print "Removing *.pyc files: " 
+  # Remove pyc files
+  Dir["#{target}/**/*"].each { |f| FileUtils.rm f and( print f, "\n" ) if f =~ /\.pyc$/ }
+  print "DONE\n"
 
-    if File.exist? targetzip
-      print "Removing old zip: "
-      FileUtils.rm targetzip
-      print "DONE\n"
-    end
-    print "Compressing: "
-    `"#{$sz}" a -mx9 "#{targetzip}" "#{target}"`
-    print "DONE\n"
-
-    print "Restoring logs: "
-    FileUtils.mv( File.join( out, "logs" ), target ) if File.exists?( File.join( out, "logs" ) )
-    print "DONE\n"
-    
-    print "\n\n"
-  else
-    print "Cleaning up *.pyc: "
-    target = File.expand_path ".."
-    targetzip = target + ".zip"
-    Dir['conversion_files/**/*'].each { |f| FileUtils.rm f and print( f, "\n" ) if f =~ /\.pyc$/ }
-    print "DONE\n"
-
-    print "Creating zip: "
-    `"#{$sz}" -a -mx9 "#{targetzip}" "in/" "conversion_files/" "doc/" ".git/" "*.rb" "README.md" "*.bat" "Gemfile" ".gitignore"`
+  if File.exist? targetzip
+    print "Removing old zip: "
+    FileUtils.rm targetzip
     print "DONE\n"
   end
-rescue
-  print "Exception: ", $@.to_s, "\n"
+  print "Compressing: "
+  `"#{$sz}" a -mx9 "#{targetzip}" "#{target}"`
+  print "DONE\n"
+
+  print "Restoring logs: "
+  FileUtils.mv( File.join( out, "logs" ), target ) if File.exists?( File.join( out, "logs" ) )
+  print "DONE\n"
+  
+  print "\n\n"
+else
+  target = File.expand_path $outputfolder
+  targetzip = target + "/selenium_wrapper-" + $rev + ".zip"
+
+  print "Cleaning up *.pyc: "
+  Dir['conversion_files/**'].each { |f| FileUtils.rm f and print( f, "\n" ) if f =~ /\.pyc$/ }
+  print "DONE\n"
+
+  if File.exist? targetzip
+    print "Removing old zip: "
+    FileUtils.rm targetzip
+    print "DONE\n"
+  end
+
+  print "Creating zip: "
+  `"#{$sz}" a -mx9 "#{targetzip}" "in/" "conversion_files/" "doc/" ".git/" "*.rb" "README.md" "*.bat" "Gemfile" ".gitignore"`
+  print "DONE\n"
 end
 
 print "Press return to finish\n"
